b782782f62e43ee300f62f1e1ad77772adea883b
Sodeeq Olalekan Adeosun <sodeeqadeosun@gmail.com>
2025-09-09 16:51:44 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="footer bg-white fixed w-screen bottom-0 py-3 ">
       <p className="text-center text-sm">
-        © {new Date().getFullYear()} | All right reserved | DFI Project
+        © {new Date().getFullYear()} | All right reserved | Adeosun Sodeeq Olalekan
       </p>
     </footer>
   );
